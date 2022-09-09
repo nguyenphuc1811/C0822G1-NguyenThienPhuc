e06eed0422876d4e5342ae1c155acdd673772678
nguyenphuc1811 <nguyenthienphuc1811@gmail.com>
2022-09-09 17:06:02 +0700

    thuc hanh ss7

--- module1/ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/su_dung_toan_tu.js
+++
@@ -1,8 +0,0 @@
-let chieudai;
-let chieucao;
-chieudai = prompt("Enter the width");
-chieucao = prompt("Enter the height");
-let width = parseInt(chieudai);
-let height = parseInt(chieucao);
-let area = width * height;
-alert("The area is: " + area);
